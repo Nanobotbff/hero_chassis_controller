921d8b5b77bd888dfb7c4f4cb9044bb5d84fd299
Nanobotbff <3225724826@qq.com>
2023-12-22 03:04:04 +0800

    Add an example.

--- src/chassis_controller_node.cpp
+++ src/chassis_controller_node.cpp
@@ -52,3 +52,104 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+//example:
+//#include "PID.h"
+//PID::PID()
+//{
+//
+//}
+//
+//void PID::update(int min_val, int max_val, float kp, float ki, float kd)
+//{
+//    min_val_ = min_val;
+//    max_val_ = max_val;
+//    kp_ = kp;
+//    ki_ = ki;
+//    kd_ = kd;
+//}
+//
+//int PID::compute(int setpoint, int measured_value)
+//{
+//
+//    double error = 0;
+//    double pid = 0;
+//
+//    //setpoint is constrained between min and max to prevent pid from having too much error
+//    if(setpoint == 0){
+//        integral_ = 0;
+//        derivative_ = 0;
+//        prev_error_ = 0;
+//        return 0;
+//    }
+//    error = setpoint - measured_value;
+//    if(abs(error)<0.1)error=0;
+//    integral_ += error;
+//    derivative_ = error - prev_error_;
+//    if(setpoint == 0 && error == 0){
+//        integral_ = 0;
+//    }
+//    pid = (kp_ * error) + (ki_ * integral_) + (kd_ * derivative_);
+//    prev_error_ = error;
+//
+//    return constrain(pid, min_val_, max_val_);
+//}
+//void PID::updateConstants(float kp, float ki, float kd)
+//{
+//    kp_ = kp;
+//    ki_ = ki;
+//    kd_ = kd;
+//}
+//PID motor1_pid;
+//PID motor2_pid;
+//PID motor3_pid;
+//PID motor4_pid;
+//void setPidParam(void)
+//{
+//    switch(configParam.RobotType){
+//        case 1:{//d2 t2
+//            motor1_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M1.K_p, configParam.p_M1.K_i,configParam.p_M1.K_d);
+//            motor2_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M2.K_p, configParam.p_M2.K_i,configParam.p_M2.K_d);
+//        }break;
+//        case 3:{//a1 转向舵机+两个减速电机
+//            motor1_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M1.K_p, configParam.p_M1.K_i,configParam.p_M1.K_d);
+//            motor2_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M2.K_p, configParam.p_M2.K_i,configParam.p_M2.K_d);
+//        }break;
+//        case 4:{//a2 转向舵机+一个动力电机
+//            if(MotorType_t == M_ESC_ENC){
+//                motor1_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                                  configParam.p_M1.K_p, configParam.p_M1.K_i,configParam.p_M1.K_d);
+//            }
+//        }break;
+//        case 5:{//o3
+//            motor1_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M1.K_p, configParam.p_M1.K_i,configParam.p_M1.K_d);
+//            motor2_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M2.K_p, configParam.p_M2.K_i,configParam.p_M2.K_d);
+//            motor3_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M3.K_p, configParam.p_M3.K_i,configParam.p_M3.K_d);
+//        }break;
+//        case 2: //d4 t4
+//        case 6: //o4
+//        case 7:{//m4
+//            motor1_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M1.K_p, configParam.p_M1.K_i,configParam.p_M1.K_d);
+//            motor2_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M2.K_p, configParam.p_M2.K_i,configParam.p_M2.K_d);
+//            motor3_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M3.K_p, configParam.p_M3.K_i,configParam.p_M3.K_d);
+//            motor4_pid.update(-configParam.Max_PWM, configParam.Max_PWM,
+//                              configParam.p_M4.K_p, configParam.p_M4.K_i,configParam.p_M4.K_d);
+//        }break;
+//    }
+//}
+//
+//
+//mDeb_p.M1.Pwm_Out  =  motor1_pid.compute(mDeb_p.M1.Expectations,mDeb_p.M1.Feedback);
+//mDeb_p.M2.Pwm_Out  =  motor2_pid.compute(mDeb_p.M2.Expectations,mDeb_p.M2.Feedback);
+//mDeb_p.M3.Pwm_Out  =  motor3_pid.compute(mDeb_p.M3.Expectations,mDeb_p.M3.Feedback);
+//mDeb_p.M4.Pwm_Out  =  motor4_pid.compute(mDeb_p.M4.Expectations,mDeb_p.M4.Feedback);
